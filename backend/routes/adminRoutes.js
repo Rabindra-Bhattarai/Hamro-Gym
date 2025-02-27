c468da8309c8d8e1b3c13217e6d7460e4ebee0d3
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2025-02-28 02:04:04 +0545

    frontend and api connection login/register

--- backend/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminController');
-
-router.post('/login', adminController.login);
-router.get('/details', adminController.getAdminDetails); // New route to get admin details
-router.delete('/delete', adminController.deleteAdmin); // New route to delete admin
-
-module.exports = router; // Ensure you are exporting the router
